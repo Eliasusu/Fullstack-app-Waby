6ae1dc9175c21ffc22b4a145c60a303c98fbb753
Eliasusu <eliasrenepalamidessi@gmail.com>
2024-05-19 18:37:12 -0300

    .

--- server/index.js
+++ server/index.js
@@ -1,15 +1,64 @@
 import express from 'express';
+import process from 'process';
+import wabys from './wabys.json' with { type: 'json' };
 
 
 // eslint-disable-next-line no-undef
 const port = process.env.PORT ?? 3000;
 const app = express();
-
+app.disable('x-powered-by');
+app.use(express.json());
 
 app.get('/', (req, res) => {
-    res.send('¡Hola, mundo!');
+    res.status(200)
+    res.send('Waby app');
 });
 
+//Get de todos los wabys
+app.get('/api/v1/wabys', (req, res) => {
+    res.json(wabys);
+});
+
+app.get('/api/v1/wabys/:id', (req, res) => {
+    const { id } = req.params;
+    const waby = wabys.find((w) => w.id === id);
+    if (waby) {
+        res.json(waby);
+    } else {
+        res.status(404).json({ error: 'Waby not found' });
+    }
+});
+
+app.post('/api/v1/wabys', (req, res) => {
+    const { 
+        nombre, 
+        mail, 
+        peso, 
+        altura, 
+        fechaNacimiento, 
+        contraseña, 
+        tipoEntrenamiento } = req.body;
+
+    //Estoy guardando el estado de un waby en memoria y eso no es muy REST que digamos
+    //Pero para el ejemplo, nos sirve
+    wabys.push({
+        id: wabys.length + 1,
+        nombre,
+        mail,
+        peso,
+        altura,
+        fechaNacimiento,
+        contraseña,
+        tipoEntrenamiento
+    });
+
+    
+    res.status(201).send('Waby created');
+    res.json(wabys);
+});
+
+
+
 app.listen(port, () => {
-    console.log(`Servidor escuchando en el puerto http://localhost/${port}`);
+    console.log(`Servidor escuchando en el puerto http://localhost:${port}`);
 });
