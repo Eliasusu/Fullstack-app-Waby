65abe773a4e1a7856ce1d39ee08652e24380e04a
Elias <eliasrenepalamidessi@gmail.com>
2024-06-14 10:14:04 -0300

    Instalacion tailwind css

---
+++ Front end/tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        'grey-nav': '#101013',
+      },
+    },
+  },
+  plugins: [],
+}
