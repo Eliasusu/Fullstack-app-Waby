1f158bccab64cef5938ea5e8782d6a520d68e04a
Elias <eliasrenepalamidessi@gmail.com>
2024-06-24 17:14:38 -0300

    .

--- src/index.js
+++ src/index.js
@@ -3,6 +3,13 @@ import process from 'process';
 import { usersRouter } from "./routes/users.routes.mjs";
 import { exercisesRouter } from "./routes/exercises.routes.mjs";
 import { corsMiddleware } from './middlewares/cors.mjs';
+//import { trainingsRouter } from './routes/trainings.routes.mjs';
+//import { indexRouter } from './routes/index.routes.mjs';
+//import { authRouter } from './routes/auth.routes.mjs';
+//import { routinesRouter } from './routes/routines.routes.mjs';
+//import { muscleGroupsRouter } from './routes/muscleGroups.routes.mjs';
+//import { mesocyclesRouter } from './routes/mesocycles.routes.mjs';
+
 
 
 // eslint-disable-next-line no-undef
@@ -15,7 +22,12 @@ app.use(corsMiddleware());
 
 app.use('/exercises', exercisesRouter);
 app.use('/users', usersRouter);
-
+//app.use('/trainings', trainingsRouter);
+//app.use('/index', indexRouter);
+//app.use('/auth', authRouter);
+//app.use('/routines', routinesRouter);
+//app.use('/muscleGroups', muscleGroupsRouter);
+//app.use('/mesocycles', mesocyclesRouter);
 
 
 app.listen(port, () => {
