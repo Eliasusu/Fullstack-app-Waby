91516fbea1f95be502bcdfd31602e549fb6002b6
FR4NKK0 <francoliberaca@gmail.com>
2024-08-03 19:57:44 -0300

    CRUD de MuscleGroup (falta testing)

--- api/muscleGroups/muscleGroups.controller.ts
+++ api/muscleGroups/muscleGroups.controller.ts
@@ -1,31 +1,65 @@
 import { Request, Response } from 'express';
 import { validateMuscleGroup, validatePartialMuscleGroup } from './muscleGroups.schema.js';
 import { MuscleGroup } from './muscleGroup.entity.js';
+import { orm } from '../shared/db/orm.js';
 
-
+const em = orm.em
 //Get all muscle groups
 async function getAll(req: Request, res: Response) {
-    res.status(500).json({ message: 'Not implemented' });
+    try{
+        const muscleGroups = await em.find(MuscleGroup, {}, { populate: ['exercises'] });
+        res
+            .status(200)
+            .json({message: 'finded all muscleGroups', muscleGroups});
+    }catch(err:any){
+        res
+            .status(500)
+            .json({message: err.message});
+    }
 }
 
 //Get one muscle group by id
 async function getOne(req: Request, res: Response) {
-    res.status(500).json({ message: 'Not implemented' });
+    try{ 
+        const idMuscleGroup = Number.parseInt(req.params.idMuscleGroup);
+        const muscleGroup = await em.findOneOrFail(MuscleGroup, {idMuscleGroup}, { populate: ['exercises'] });
+        res.status(200).json({message: 'finded muscleGroup', muscleGroup});
+    } catch(err:any){
+        res.status(500).json({message: err.message})};
 }
 
 //Add a muscle group
 async function add(req: Request, res: Response) {
-    res.status(500).json({ message: 'Not implemented' });
+    try{ 
+        const muscleGroup = em.create(MuscleGroup, req.body.sanitizeInput);
+        await em.flush()
+        res.status(201).json({message: 'MuscleGroup created', muscleGroup});
+    } catch(err:any){
+        res.status(500).json({message: err.message})};
 }
 
 //Update a muscle group
 async function update(req: Request, res: Response) {
-    res.status(500).json({ message: 'Not implemented' });
+    try {
+        const idMuscleGroup = Number.parseInt(req.params.idMuscleGroup);
+        const muscleGroupToUpdate = await em.findOneOrFail(MuscleGroup, { idMuscleGroup });
+        em.assign(muscleGroupToUpdate, req.body.sanitizeInput);
+        await em.flush();
+        res.status(202).json({message: 'MuscleGroup updated succesfully'});
+    } catch(err:any){
+        res.status(500).json({message: err.message});}
 }
 
 //Delete a muscle group
 async function remove(req: Request, res: Response) {
-    res.status(500).json({ message: 'Not implemented' });
+    try{
+        const idMuscleGroup = Number.parseInt(req.params.idMuscleGroup);
+        const muscleGroup = em.getReference(MuscleGroup, idMuscleGroup as never);
+        await em.removeAndFlush(muscleGroup);
+        res.status(204).json({message: 'MuscleGroup deleted succesfully'});
+    }catch(err:any){
+        res.status(500).json({message: err.message});
+    };
 }
 
 export { getAll, getOne, add, update, remove }
