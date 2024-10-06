dcda9445571a3a376416f5a5998d6a13076af7d9
FR4NKK0 <francoliberaca@gmail.com>
2024-10-06 16:42:19 -0300

    .

--- client/src/exercises/exercise.api.ts
+++ client/src/exercises/exercise.api.ts
@@ -1,4 +1,5 @@
 import axios from '@/lib/axios';
+import { Exercise } from './exercise.type';
 
 export const createExercise = (exercise: object) => axios.post('/exercises', exercise)
 
@@ -7,3 +8,5 @@ export const getExercisesReq = () => axios.get('/exercises')
 export const getExercisesByMg = (mg: string) => axios.get(`/exercises/muscleGroup/${mg}`)
 
 export const deleteExercise = (id: number) => axios.delete(`/exercises/${id}`)
+
+export const updateExercise = (exercise: Exercise) => axios.put(`/exercises/${exercise.idExercise}`, exercise)
