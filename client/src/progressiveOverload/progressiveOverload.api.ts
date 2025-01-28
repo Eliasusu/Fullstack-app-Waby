b9c87a79fd4a0a2803fea0d48fcd2edf61253dab
Elias <eliasrenepalamidessi@gmail.com>
2025-01-28 15:36:20 -0300

    Get one progressive Overload

--- client/src/progressiveOverload/progressiveOverload.api.ts
+++ client/src/progressiveOverload/progressiveOverload.api.ts
@@ -5,10 +5,10 @@ export const createProgressiveOverload = (progressiveOverload: object) => axios.
 
 export const getProgressiveOverloadsReq = () => axios.get('/progressiveOverload')
 
+export const getProgressiveOverload = (id: number) => axios.get(`/progressiveOverload/${id}`)
+
 export const deleteProgressiveOverload = (id: number) => axios.delete(`/progressiveOverload/${id}`)
 
 export const updateProgressiveOverload = (progressiveOverload: ProgressiveOverload) => axios.put(`/progressiveOverload/${progressiveOverload.idProgressiveOverload}`, progressiveOverload)
-
-export const getProgressiveOverloadsByUser = (id: number) => axios.get(`/progressiveOverload/user/${id}`)
 
 export const getProgressiveOverloadsByExercise = (id: number) => axios.get(`/progressiveOverload/exercise/${id}`)
